cc026baa0bb66d55a4db6fbe0b2fc66c4c2e1c98
Prithvi Pal Singh <Prithvi.Coer@outlook.com>
2018-05-18 11:19:09 +0800

    Largest prime factor of a number

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -11,8 +11,14 @@ namespace ProjectEuler
     {
         static void Main(string[] args)
         {
-            FindSum();
-
+            //FindSum();
+            //EvenSum();
+            LargestPrimeFactor();
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    Console.WriteLine(i + " is prime: " + IsPrime(i));
+            //}
+            //Console.WriteLine(IsPrime(17));
             Console.Read();
         }
 
@@ -22,10 +28,10 @@ namespace ProjectEuler
         /// <param name="n"></param>
         static void FindSum()
         {
-            int t = Convert.ToInt32(Console.ReadLine());
+            int t = 1;// Convert.ToInt32(Console.ReadLine());
             for (int a0 = 0; a0 < t; a0++)
             {
-                long n = 100;// Convert.ToInt32(Console.ReadLine());
+                long n = 10000000000000;// Convert.ToInt32(Console.ReadLine());
                 long sum = 0;
                 long p = (n - 1) / 3;
                 sum = ((3 * p * (p + 1)) / 2);
@@ -37,6 +43,77 @@ namespace ProjectEuler
                 sum = sum - ((15 * p * (p + 1)) / 2);
                 Console.WriteLine(sum);
             }
+        }
+
+        static void EvenSum()
+        {
+            long sum = 0;
+
+            long a = 1;
+            for (long b = 2; b <= 100;)
+            {
+                if (b % 2 == 0)
+                {
+                    sum += b;
+                }
+
+                long c = a + b;
+                a = b;
+                b = c;
+            }
+
+            Console.WriteLine(sum);
+        }
+
+        static void LargestPrimeFactor()
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            for (int a0 = 0; a0 < t; a0++)
+            {
+                double n = Convert.ToInt64(Console.ReadLine());
+                Console.WriteLine(FindLargestPrime(n));
+            }
+        }
+
+        private static double FindLargestPrime(double n)
+        {
+            double max = -1;
+            while (n % 2 == 0)
+            {
+                max = 2;
+                n = n / 2;
+            }
+
+            for (int i = 3; i <= Math.Sqrt(n); i += 2)
+            {
+                while (n % i == 0)
+                {
+                    max = i;
+                    n = n / i;
+                }
+            }
+
+            if (n > 2)
+                max = n;
+
+            return max;
+        }
+
+        public static bool IsPrime(double number)
+        {
+            if (number == 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
         }
     }
 }
